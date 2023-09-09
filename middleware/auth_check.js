74c96eda680893c7ab0050adb20f99a1a69e0355
Ashak Zahin Hasan <18-36415-1@student.aiub.edu>
2023-09-09 20:34:18 +0600

    feat: add functionality into token and admin validity check middleware

--- middleware/auth_check.js
+++ middleware/auth_check.js
@@ -2,15 +2,62 @@ const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 
+/**
+ * @namespace AuthCheckMiddleware
+ */
 
+/**
+ * @memberof AuthCheckMiddleware
+ * @async
+ * @method
+ * @description Checking token is valid or not.
+ * @param {object} req - request object.
+ * @param {object} res - response object.
+ * @param {function} next - next function.
+ * @requires ../models/user.model
+ * @requires ../config/config
+ * @returns {JSON} - if user token in valid then function will redirect to next function else unauthorized error.
+ */
+const checkTokenValidity = async (req, res, next) => {
+  if (!req.cookies.token) {
+    return res.status(403).json({
+      success: false,
+      message: "No token found.",
+    });
+  }
 
-const checkTokenExpiration = async (req, res, next) => {
-  next()
+  jwt.verify(req.cookies.token, config.JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      //token verification failed
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    res.locals.userId = decodedToken._id; //passing user id to next() function. In our case it's checkAuthorization()
+    next();
+  });
 };
 
+
+/**
+ * @memberof AuthCheckMiddleware
+ * @async
+ * @method
+ * @description Checking user is admin or not.
+ * @param {object} req - request object.
+ * @param {object} res - response object.
+ * @param {function} next - next function.
+ * @requires ../models/user.model
+ * @requires ../config/config
+ * @returns {JSON} - if user is admin then function will redirect to next function else unauthorized error.
+ */
 const checkAuthorization = async (req, res, next) => {
-  next()
+  const user = await User.findById(res.locals.userId);
+  if (!user.is_admin) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized to access" });
+  }
+  next();
 };
 
-
-module.exports = { checkTokenExpiration, checkAuthorization };
+module.exports = { checkTokenValidity, checkAuthorization };
